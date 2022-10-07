505106cfc410d35b941220419f175d4b9de1e242
dev-michel324 <dev-michel324@protonmail.com>
2022-10-07 20:58:18 -0300

    feat: added feature to save data received from post method

--- posts/views.py
+++ posts/views.py
@@ -6,12 +6,18 @@ from rest_framework import status
 from .serializers import PostSerializer
 from .models import Post
 
-@api_view(["GET"])
+@api_view(['GET'])
 def post_list(request):
-    if request.method == 'GET':
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
         return JsonResponse(
             {'posts': serializer.data},
             safe=False
         )
+
+@api_view(['POST'])
+def post_create(request):
+    serializer = PostSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
