ca5c4230cacef40e147d0cb6482a0fb812ba6062
dev-michel324 <dev-michel324@protonmail.com>
2022-10-07 21:37:45 -0300

    fix: the function dont return a error response when the data is invalid

--- posts/views.py
+++ posts/views.py
@@ -21,6 +21,7 @@ def post_create(request):
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def post_detail(request, id):
